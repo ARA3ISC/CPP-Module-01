eb810c3294b74f060588b28f2f1a04190e7f6df3
ara3isc <m.aneddame01@gmail.com>
2023-09-07 17:16:23 +0100

    .

--- ex04/replace.cpp
+++ ex04/replace.cpp
@@ -2,15 +2,10 @@
 
 void	writeToFile(std::string fileName, std::string line, std::string s1, std::string s2) {
 
-	(void)fileName;
 	std::fstream fileObj;
 
-	// std::cout << line << std::endl;
-	// return; 
 	size_t index = line.find(s1);
 	while (index != std::string::npos) {
-
-	// std::cout << "***" << std::endl;
 	
 		line.erase(index, s1.length());
 		line.insert(index, s2);
@@ -18,10 +13,11 @@ void	writeToFile(std::string fileName, std::string line, std::string s1, std::st
 			break;
 		index = line.find(s1);
 	};
-	// std::cout << line << std::endl;
 	fileObj.open((fileName + ".replace").c_str(), std::ios::out | std::ios::app);
 	if (fileObj.is_open())
 		fileObj << line << std::endl;
+
+	fileObj.close();
 }
 
 void	readFromFile(std::string fileName, std::string s1, std::string s2) {
@@ -36,7 +32,7 @@ void	readFromFile(std::string fileName, std::string s1, std::string s2) {
 		while (getline(fileObj, line)) {
 			writeToFile(fileName, line, s1, s2);
 		}
-		return;
+		fileObj.close();
 	}
 	else
 		std::cout << "error\n";
