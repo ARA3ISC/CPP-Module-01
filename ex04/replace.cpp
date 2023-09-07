4700e76fb00013fce54b527c4833a20eb2acd38a
ara3isc <m.aneddame01@gmail.com>
2023-09-07 16:50:28 +0100

    fixed replacing all occurences

--- ex04/replace.cpp
+++ ex04/replace.cpp
@@ -2,30 +2,41 @@
 
 void	writeToFile(std::string fileName, std::string line, std::string s1, std::string s2) {
 
+	(void)fileName;
 	std::fstream fileObj;
 
+	// std::cout << line << std::endl;
+	// return; 
 	size_t index = line.find(s1);
-	if (index != std::string::npos) {
+	while (index != std::string::npos) {
+
+	// std::cout << "***" << std::endl;
+	
 		line.erase(index, s1.length());
 		line.insert(index, s2);
-	}
-	fileObj.open(fileName + ".replace", std::ios::out | std::ios::app);
+		if (index == std::string::npos)
+			break;
+		index = line.find(s1);
+	};
+	// std::cout << line << std::endl;
+	fileObj.open((fileName + ".replace").c_str(), std::ios::out | std::ios::app);
 	if (fileObj.is_open())
 		fileObj << line << std::endl;
 }
 
 void	readFromFile(std::string fileName, std::string s1, std::string s2) {
 
+	(void)s2;
+
 	std::string line;
 	std::fstream fileObj;
 
-	fileObj.open(fileName, std::ios::in);
+	fileObj.open(fileName.c_str(), std::ios::in);
 	if (fileObj.is_open()) {
-		while (1) {
-			if (getline(fileObj, line).eof())
-				break;
+		while (getline(fileObj, line)) {
 			writeToFile(fileName, line, s1, s2);
 		}
+		return;
 	}
 	else
 		std::cout << "error\n";
